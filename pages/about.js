5732781de48a763ab03c7ac4ca34d3a52fb67d8b
unknown <bentaouiloussama@gmail.com>
2022-04-05 15:33:24 +0000

    Layout, Header, Footer

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,14 @@
+import Link from "next/dist/client/link";
+import Layout from "./components/layout";
 export default function AboutPage() {
   return (
-    <div>
+    <Layout title="About DJ Events">
       <h1>About</h1>
       <p>This is an app to find the latest DJ and other musical events</p>
       <p>Version: 1.0.0</p>
-    </div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </Layout>
   );
 }
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,8 @@
+import Layout from "../components/layout";
 export default function EventPage() {
   return (
-    <div>
+    <Layout>
       <h1>My Event</h1>
-    </div>
+    </Layout>
   );
 }
